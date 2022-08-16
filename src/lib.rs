c7e27fc44f7495f0129f75fea3b6ff318d40d61e
Brad Hesson <brad.hesson@outlook.com>
2022-08-16 02:04:46 -0600

    add traits to serde types using a format I choose

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,22 @@ pub struct HttpPostData {
 pub struct HttpRespData {
     pub success: bool,
 }
+
+pub trait SerializeBytes
+where
+    Self: Serialize,
+{
+    fn as_bytes(&self) -> Option<Vec<u8>> {
+        ron::ser::to_string(self).ok().map(String::into)
+    }
+}
+impl<T> SerializeBytes for T where T: Serialize {}
+pub trait DeserializeBytes<'a>
+where
+    Self: Deserialize<'a>,
+{
+    fn from_bytes(bytes: &'a [u8]) -> Option<Self> {
+        ron::de::from_bytes(bytes).ok()
+    }
+}
+impl<'de, T> DeserializeBytes<'de> for T where T: Deserialize<'de> {}

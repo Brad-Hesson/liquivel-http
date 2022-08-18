b14539c012977c50ae65996a479b2a937b3a7448
Brad Hesson <brad.hesson@outlook.com>
2022-08-18 15:43:26 -0600

    changed http message format

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,42 @@
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug, Serialize, Clone)]
-pub struct HttpPostData {
-    pub reading: Vec<i16>,
-    pub time: std::time::SystemTime,
-    pub uuid: u16,
-    pub battery_level: f32,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum HttpMessage {
+    HttpReading {
+        reading: Vec<i16>,
+        time: std::time::SystemTime,
+        uuid: u16,
+        battery_level: f32,
+    },
+    HttpAck,
+    Error {
+        error: String,
+        time: std::time::SystemTime,
+    },
 }
-
-#[derive(Deserialize, Debug, Serialize, Clone)]
-pub struct HttpRespData {
-    pub success: bool,
-}
-
-pub trait SerializeBytes
-where
-    Self: Serialize,
-{
-    fn as_bytes(&self) -> Option<Vec<u8>> {
-        ron::ser::to_string(self).ok().map(String::into)
+impl HttpMessage {
+    pub fn serialize(&self) -> Vec<u8> {
+        // ron::ser::to_string(self).expect("serialization should never fail").into()
+        postcard::to_allocvec(self).expect("serialization should never fail")
+    }
+    pub fn deserialize(bytes: &[u8]) -> Result<Self> {
+        let data = postcard::from_bytes(bytes)?;
+        // let data = ron::de::from_bytes(bytes)?;
+        Ok(data)
     }
 }
-impl<T> SerializeBytes for T where T: Serialize {}
-pub trait DeserializeBytes<'a>
-where
-    Self: Deserialize<'a>,
-{
-    fn from_bytes(bytes: &'a [u8]) -> Option<Self> {
-        ron::de::from_bytes(bytes).ok()
+
+#[test]
+fn serialization() {
+    // let bytes = HttpMessage::Error.serialize();
+    let bytes = HttpMessage::HttpReading {
+        reading: vec![0, 1, 4, 9],
+        time: std::time::SystemTime::now(),
+        uuid: 5,
+        battery_level: 4.3356,
     }
+    .serialize();
+    println!("{:?}", bytes);
+    println!("{}", String::from_utf8_lossy(&bytes[..]));
 }
-impl<'de, T> DeserializeBytes<'de> for T where T: Deserialize<'de> {}

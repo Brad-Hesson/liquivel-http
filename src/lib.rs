c60f01e7263fe0fb9784d483c54282afd614c3b7
Brad Hesson <brad.hesson@outlook.com>
2022-08-19 20:28:51 -0600

    changed messages

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ pub enum HttpMessage {
     Reading {
         reading: Vec<i16>,
         time: std::time::SystemTime,
-        uuid: u16,
-        battery_level: f32,
     },
     Ack,
     Error {
@@ -28,30 +26,3 @@ pub fn deserialize(bytes: &[u8]) -> Result<Vec<HttpMessage>> {
     let data = postcard::from_bytes(bytes)?;
     Ok(data)
 }
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// pub struct HttpTransaction(pub Vec<HttpMessage>);
-// impl HttpTransaction {
-//     pub fn serialize(&self) -> Vec<u8> {
-//         // ron::ser::to_string(self).expect("serialization should never fail").into()
-//         postcard::to_allocvec(self).expect("serialization should never fail")
-//     }
-//     pub fn deserialize(bytes: &[u8]) -> Result<Self> {
-//         // let data = ron::de::from_bytes(bytes)?;
-//         let data = postcard::from_bytes(bytes)?;
-//         Ok(data)
-//     }
-// }
-
-// #[test]
-// fn serialization() {
-//     // let bytes = HttpMessage::Error.serialize();
-//     let bytes = HttpTransaction(vec![HttpMessage::Reading {
-//         reading: vec![0, 1, 4, 9],
-//         time: std::time::SystemTime::now(),
-//         uuid: 5,
-//         battery_level: 4.3356,
-//     }])
-//     .serialize();
-//     println!("{:?}", bytes);
-//     println!("{}", String::from_utf8_lossy(&bytes[..]));
-// }

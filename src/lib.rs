997e3945d98b9bfa55823f4f2e67388ecff14067
Brad Hesson <brad.hesson@outlook.com>
2022-08-21 23:33:52 -0600

    changed message to be a packet

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,35 @@ use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum HttpMessage {
-    Reading {
-        reading: Vec<i16>,
-        time: std::time::SystemTime,
-    },
-    Ack,
-    Error {
-        error: String,
-        time: std::time::SystemTime,
-    },
-    Status {
-        status: String,
-        time: std::time::SystemTime,
-    },
-    SetHibernateTime{
-        ms: u32
-    },
-    SetNumReadings{
-        num: u32
+pub struct HttpPacket {
+    pub time: std::time::SystemTime,
+    pub uuid: u32,
+    pub message: HttpMessage,
+}
+impl HttpPacket {
+    pub fn new(uuid: u32, message: HttpMessage) -> Self {
+        Self {
+            time: std::time::SystemTime::now(),
+            uuid,
+            message,
+        }
     }
 }
-pub fn serialize(messages: &[HttpMessage]) -> Vec<u8> {
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum HttpMessage {
+    Reading(Vec<i16>),
+    Ack,
+    Error(String),
+    Status(String),
+    SetHibernateTime(u32),
+    SetNumReadings(u32),
+}
+pub fn serialize(messages: &[HttpPacket]) -> Vec<u8> {
     // ron::ser::to_string(self).expect("serialization should never fail").into()
     postcard::to_allocvec(messages).expect("serialization should never fail")
 }
-pub fn deserialize(bytes: &[u8]) -> Result<Vec<HttpMessage>> {
+pub fn deserialize(bytes: &[u8]) -> Result<Vec<HttpPacket>> {
     // let data = ron::de::from_bytes(bytes)?;
     let data = postcard::from_bytes(bytes)?;
     Ok(data)
